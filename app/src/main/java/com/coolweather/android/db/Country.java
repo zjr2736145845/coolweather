c8ccb5bfc612ec389ae5883b745efb1ab2e3a309
zjr2736145845 <yourname>
2024-05-27 11:33:47 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/coolweather/android/db/Country.java
@@ -0,0 +1,34 @@
+package com.coolweather.android.db;
+
+
+import org.litepal.crud.LitePalSupport;
+
+public class Country extends LitePalSupport {
+    private int id;
+    private String proviceName;
+    private int proviceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProviceName() {
+        return proviceName;
+    }
+
+    public void setProviceName(String proviceName) {
+        this.proviceName = proviceName;
+    }
+
+    public int getProviceCode() {
+        return proviceCode;
+    }
+
+    public void setProviceCode(int proviceCode) {
+        this.proviceCode = proviceCode;
+    }
+}
